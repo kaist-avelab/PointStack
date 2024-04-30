1fda5aaa5c3214f85526460be411b35bb03a248d
Prof. Seung-Hyun Kong <93636289+kaist-avelab@users.noreply.github.com>
2024-04-30 13:24:43 +0900

    Add files via upload

--- utils/vis_utils.py
+++ utils/vis_utils.py
@@ -24,61 +24,21 @@ def visualize_numpy(pc_numpy, colors = None):
     
     # o3d.visualization.draw_geometries([point_cloud])
     
-def visualize_part(net, testloader):
-    color_choices = [
-        (1, 0, 0),      # Red
-        (0, 1, 0),      # Green
-        (0, 0, 1),      # Blue
-        (1, 1, 0),      # Yellow
-        (0, 1, 1),      # Cyan
-        (1, 0, 1),      # Magenta
-        (1, 0.5, 0),    # Orange
-        (0.5, 0, 0.5),  # Purple
-        (0, 0.5, 0.5),  # Teal
-        (0.5, 0.5, 0),  # Olive
-        (0.5, 0, 0),    # Brown
-        (0, 0.5, 0),    # Green (darker)
-        (0, 0, 0.5)     # Navy
-    ]
-
-    net.eval()
-
-    with torch.no_grad():
-        for batch_idx, data_dic in enumerate(tqdm(testloader)):
-            if ((batch_idx % 10 == 0) and (batch_idx > 1260)):
-                data_dic = net(data_dic)        
-                points = data_dic['points'].squeeze(0).cpu().numpy()
-                label = data_dic['seg_id'].squeeze(0).cpu().numpy()
-                pred = torch.argmax(data_dic['pred_score_logits'], dim = -1).squeeze(0).cpu().numpy()
-
-                color_list = np.zeros_like(points)
-                for i, pred_id in enumerate(pred):
-                    color_list[i] = color_choices[int(pred_id)]            
-                visualize_numpy(points, colors=color_list)
-
-                color_list = np.zeros_like(points)
-                for i, label_id in enumerate(label):
-                    color_list[i] = color_choices[int(label_id)]            
-                visualize_numpy(points, colors=color_list)
-
-
-# def visualize_part(net, testloader): # for shapenet part
-#     color_choices = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #3
-#                      (1, 0, 0), (0, 1, 0), #5
-#                      (1, 0, 0), (0, 1, 0), #7
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #11
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #15
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), # 18
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #21
-#                      (1, 0, 0), (0, 1, 0), #23
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #27
-#                      (1, 0, 0), (0, 1, 0), #29
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (0, 1, 1), (1, 0, 1), # 35
-#                      (1, 0, 0), (0, 1, 0), #37
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), #40
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), #43
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), #46
-#                      (1, 0, 0), (0, 1, 0), (0, 0, 1), #49
+# def visualize_part(net, testloader):
+#     color_choices = [
+#         (1, 0, 0),      # Red
+#         (0, 1, 0),      # Green
+#         (0, 0, 1),      # Blue
+#         (1, 1, 0),      # Yellow
+#         (0, 1, 1),      # Cyan
+#         (1, 0, 1),      # Magenta
+#         (1, 0.5, 0),    # Orange
+#         (0.5, 0, 0.5),  # Purple
+#         (0, 0.5, 0.5),  # Teal
+#         (0.5, 0.5, 0),  # Olive
+#         (0.5, 0, 0),    # Brown
+#         (0, 0.5, 0),    # Green (darker)
+#         (0, 0, 0.5)     # Navy
 #     ]
 
 #     net.eval()
@@ -100,3 +60,43 @@ def visualize_part(net, testloader):
 #                 for i, label_id in enumerate(label):
 #                     color_list[i] = color_choices[int(label_id)]            
 #                 visualize_numpy(points, colors=color_list)
+
+
+def visualize_part(net, testloader): # for shapenet part
+    color_choices = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #3
+                     (1, 0, 0), (0, 1, 0), #5
+                     (1, 0, 0), (0, 1, 0), #7
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #11
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #15
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), # 18
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #21
+                     (1, 0, 0), (0, 1, 0), #23
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), #27
+                     (1, 0, 0), (0, 1, 0), #29
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (0, 1, 1), (1, 0, 1), # 35
+                     (1, 0, 0), (0, 1, 0), #37
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), #40
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), #43
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), #46
+                     (1, 0, 0), (0, 1, 0), (0, 0, 1), #49
+    ]
+
+    net.eval()
+
+    with torch.no_grad():
+        for batch_idx, data_dic in enumerate(tqdm(testloader)):
+            if ((batch_idx % 10 == 0) and (batch_idx > 1260)):
+                data_dic = net(data_dic)        
+                points = data_dic['points'].squeeze(0).cpu().numpy()
+                label = data_dic['seg_id'].squeeze(0).cpu().numpy()
+                pred = torch.argmax(data_dic['pred_score_logits'], dim = -1).squeeze(0).cpu().numpy()
+
+                color_list = np.zeros_like(points)
+                for i, pred_id in enumerate(pred):
+                    color_list[i] = color_choices[int(pred_id)]            
+                visualize_numpy(points, colors=color_list)
+
+                color_list = np.zeros_like(points)
+                for i, label_id in enumerate(label):
+                    color_list[i] = color_choices[int(label_id)]            
+                visualize_numpy(points, colors=color_list)
